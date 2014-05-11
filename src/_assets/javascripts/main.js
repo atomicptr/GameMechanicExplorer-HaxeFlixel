593d35c4aeed65e0be592ed70fe8fe66adb24e11
Christopher Kaster <ikasoki@gmail.com>
2014-05-11 13:35:07 +0200

    improve canvas size, add fullscreen button

--- src/_assets/javascripts/main.js
+++ src/_assets/javascripts/main.js
@@ -1,12 +1,54 @@
+var should_hide = false;
+var is_hidden = false;
+
 $(document).ready(function() {
-	PLAY_STATE_PATH = "src/states/PlayState.hx";
 
-	console.log("try to get: " + PLAY_STATE_PATH);
-
-	$.get(PLAY_STATE_PATH, function(data) {
+	// load script
+	$.get("src/states/PlayState.hx", function(data) {
 		$("#code-example").html(data);
 
 		sh_highlightDocument();
 	});
 
+	// format iframe
+	var width = $('.game-container iframe').width();
+	
+	width = Math.ceil(width / 32) * 32;
+	
+	var height = Math.ceil((width * 0.4) / 32) * 32;
+	
+	$('.game-container iframe').width(width);
+	$('.game-container iframe').height(height);
+	
+	// show width, height
+	//$(".jumbotron").html(width + ", " + height);
+	
+	if(width < 600) {
+		$("#game-frame").width(1);
+		$("#game-frame").height(1);
+	}
+});
+
+// reload page on orientation change
+$(window).on('orientationchange', function(e) {
+	window.location.reload();
+});
+
+function gameframe_fullscreen() {
+
+	
+	var gameFrame = document.getElementById('game-frame');
+	
+	if(gameFrame.webkitRequestFullscreen) {
+		gameFrame.webkitRequestFullscreen();
+	} else {
+		gameFrame.mozRequestFullscreen();
+	}
+	
+	gameFrame.focus();
+}
+
+$(document).on('webkitfullscreenchange mozfullscreenchange fullscreenchange', function() {	
+	var gameFrame = document.getElementById('game-frame');
+	gameFrame.contentDocument.location.reload(true);
 });
